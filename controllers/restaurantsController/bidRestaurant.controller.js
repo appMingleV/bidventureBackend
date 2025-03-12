00fe54e803a3bf300b8cbc318c98d4e8502f5364
Rajat Appmingle <rajat.gupta@appminglemedia.com>
2025-03-12 14:31:46 +0530

    populated bid history

--- controllers/restaurantsController/bidRestaurant.controller.js
+++ controllers/restaurantsController/bidRestaurant.controller.js
@@ -8,7 +8,7 @@ import User from "../../models/users/userAuth.model.js";
 class bidRestarurant {
   async getAllEvents(req, res) {
     try {
-      const Events = await BidForm.find({ eventStatus: "Pending" });
+      const Events = await BidForm.find({ eventStatus: "Pending" }).populate("bidHistory")
       if (Events.length == 0) {
         return res.status(404).json({
           status: true,
