de1fe20159db4d74ad4936cef08504355ef86146
Rajat Appmingle <rajat.gupta@appminglemedia.com>
2025-03-18 15:56:41 +0530

    io runtime fixes

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ import { connectDB } from "./config/db.js";
 import routes from "./routes/index.js";
 import http from "http";
 import cors from "cors";
-import { initiateSocket } from "./socket.js";
+import { getIO, initiateSocket } from "./socket.js";
 connectDB();
-const app = express({ limit: "300mb" });
+const app = express();
 const server = http.createServer(app);
 server.timeout = 0; // Disable timeout
-app.use(express.json());
+app.use(express.json({ limit: "300mb" }));
 app.use(cors());
 app.use("/api/uploads", express.static("Uploads"));
 app.use("/api", routes);
 initiateSocket(server);
+const io = getIO();
 app.use((req, res, next) => {
   req.io = io;
   req.restaurantSockets = restaurantSockets;
